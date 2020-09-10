4e3d556e459de5b3a50b41511e72b73f3d03cd73
mloukopoulou <mloukopoulou@knowledge.gr>
2020-09-10 11:15:47 +0300

    fix

--- server/app.js
+++ server/app.js
@@ -5,12 +5,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const port = process.env.PORT || 5000;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 app.use(cors());
 app.options('*', cors());
+app.set('port', port);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
